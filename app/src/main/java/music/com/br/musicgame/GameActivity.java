6a74fc6620e2d4620d64181bf0d7dffbff7b458d
mtsodf <mtsodf@gmail.com>
2016-02-02 20:55:05 -0200

    Jogo Funcionando

--- app/src/main/java/music/com/br/musicgame/GameActivity.java
+++ app/src/main/java/music/com/br/musicgame/GameActivity.java
@@ -1,10 +1,17 @@
 package music.com.br.musicgame;
 
 import android.app.Activity;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import music.com.br.musicgame.entities.Jogo;
 import music.com.br.musicgame.entities.Note;
@@ -13,7 +20,11 @@ import music.com.br.musicgame.entities.NoteGame;
 public class GameActivity extends Activity {
 
     Jogo noteGame;
-
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
@@ -23,26 +34,45 @@ public class GameActivity extends Activity {
 
         noteGame = new NoteGame();
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
         getReferenceToViews();
 
 
         criarNovoJogo();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Game Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://music.com.br.musicgame/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
     }
 
     private void criarNovoJogo() {
         noteGame.newPhase();
 
-
-        questionView.setText(noteGame.currentNote.toString());
-
-
         questionView.setText(noteGame.question);
 
+        setAnswers();
+    }
+
+    private void setAnswers() {
         answer1View.setText(noteGame.currentAlternatives.get(0).toString());
         answer2View.setText(noteGame.currentAlternatives.get(1).toString());
         answer3View.setText(noteGame.currentAlternatives.get(2).toString());
@@ -55,42 +85,78 @@ public class GameActivity extends Activity {
         answer3View = (TextView) findViewById(R.id.answer3View);
     }
 
-    public void onButtonClick(View v){
+    public void onButtonClick(View v) {
 
-        if(v.getId() == R.id.answer1View){
-            if(noteGame.rightAnswer(noteGame.currentAlternatives.get(0))){
-
-            } else{
-
+        if (v.getId() == R.id.answer1View) {
+            if (noteGame.rightAnswer(noteGame.currentAlternatives.get(0))) {
+                setTextColor(v, 0);
+            } else {
+                setTextColor(v, 1);
             }
             criarNovoJogo();
             return;
         }
-        if(v.getId() == R.id.answer2View){
-            if(noteGame.rightAnswer(noteGame.currentAlternatives.get(0))){
-
-            } else{
-
-            }
-            criarNovoJogo();
-            return;
-
-        }
-        if(v.getId() == R.id.answer3View){
-            if(noteGame.rightAnswer(noteGame.currentAlternatives.get(0))){
-
-            } else{
-
+        if (v.getId() == R.id.answer2View) {
+            if (noteGame.rightAnswer(noteGame.currentAlternatives.get(1))) {
+                setTextColor(v, 0);
+            } else {
+                setTextColor(v, 1);
             }
             criarNovoJogo();
             return;
 
         }
+        if (v.getId() == R.id.answer3View) {
+            if (noteGame.rightAnswer(noteGame.currentAlternatives.get(2))) {
+                setTextColor(v, 0);
+            } else {
+                setTextColor(v, 1);
+            }
+            criarNovoJogo();
+            return;
+
+        }
 
 
+    }
+
+    private void setTextColor(View v, int i) {
+        TextView t = (TextView) v;
+
+        if (i == 0)
+            t.setTextColor(Color.RED);
+        else
+            t.setTextColor(Color.GREEN);
+
+        try {
+            wait(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        t.setTextColor(Color.BLACK);
 
     }
 
     TextView questionView, answer1View, answer2View, answer3View;
 
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Game Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://music.com.br.musicgame/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
